0a2a89841f3701a8613ddd2111d034f2cdadaf55
Elia Tolin <eliatolin@gmail.com>
2023-05-18 16:01:40 +0200

    redirect

--- pagine_gialle_scraper/pagine_gialle_scraper/urls.py
+++ pagine_gialle_scraper/pagine_gialle_scraper/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 
 admin.site.site_header = 'PagineGialleScraper Administrator'
@@ -6,6 +7,7 @@ admin.site.site_title = 'PagineGialleScraper Administrator'
 admin.site.index_title = 'PagineGialleScraper'
 
 urlpatterns = [
+    path("", lambda request: redirect('dashboard:index'), name="redirect_index"),
     path("dashboard/", include("dashboard.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
